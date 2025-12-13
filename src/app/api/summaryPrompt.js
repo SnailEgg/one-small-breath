08d9d684f65cc8896d0c228cdd374ba622c13c5a
SnailEgg <96706728+SnailEgg@users.noreply.github.com>
2025-12-12 16:52:24 -0800

    changed summaryPrompt to tell AI to include previous summaries in summary

--- src/app/api/summaryPrompt.js
+++ src/app/api/summaryPrompt.js
@@ -1,4 +1,4 @@
 const summaryPrompt =
-`Concisely summarize the previous messages to provide context for the assistant in the future.`;
+`Concisely summarize the previous summary and the following messages to provide context for the assistant in the future.`;
 
 export default summaryPrompt;
